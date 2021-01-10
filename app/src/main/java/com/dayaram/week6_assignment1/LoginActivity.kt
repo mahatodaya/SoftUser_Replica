972abdfad5dc06454842b709628260afb222f408
mahatodaya <mahatodayaram1@gmail.com>
2021-01-10 16:46:01 +0545

    Design for login page was done.

---
+++ app/src/main/java/com/dayaram/week6_assignment1/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.dayaram.week6_assignment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
