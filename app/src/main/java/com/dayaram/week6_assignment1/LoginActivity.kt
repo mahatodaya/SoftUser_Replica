ba5b6764cea5071fcf0391be7bb599a4b340653c
mahatodaya <mahatodayaram1@gmail.com>
2021-01-10 17:02:44 +0545

    Username and password for login activity was set.

--- app/src/main/java/com/dayaram/week6_assignment1/LoginActivity.kt
+++ app/src/main/java/com/dayaram/week6_assignment1/LoginActivity.kt
@@ -1,11 +1,41 @@
 package com.dayaram.week6_assignment1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var pbLogin: ProgressBar
+    private lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etUsername = findViewById(R.id.etLoginUsername)
+        etPassword = findViewById(R.id.etLoginPassword)
+        pbLogin = findViewById(R.id.pbLogin)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        val username = etUsername.text.toString().trim()
+        val password = etPassword.text.toString().trim()
+
+        if (username == "softwarica" && password == "coventry"){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        pbLogin.visibility = View.VISIBLE
     }
 }
